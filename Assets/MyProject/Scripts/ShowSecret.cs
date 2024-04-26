975678f4706fb851f824ad65fcdbccc20fa35322
MushcV2 <titi.c2006@gmail.com>
2024-04-26 10:47:43 -0300

    Secret Added

---
+++ Assets/MyProject/Scripts/ShowSecret.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowSecret : MonoBehaviour
+{
+    [SerializeField] private TMP_Text secretText;
+
+    private void Start()
+    {
+        secretText.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        if (_other.gameObject.CompareTag("Player"))
+        {
+            secretText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D _other)
+    {
+        if (_other.gameObject.CompareTag("Player"))
+        {
+            secretText.gameObject.SetActive(false);
+        }
+    }
+}
